0c99c21eabd80fb41da20e15617c314f177f9ac0
Jay Namburi <nambjay@gmail.com>
2025-05-03 16:41:56 -0500

    Add missing trafficLightZones.ts file

---
+++ src/data/trafficLightZones.ts
@@ -0,0 +1,35 @@
+/**
+ * Traffic Light Zones
+ * 
+ * This file defines the suburbs in each traffic light zone (green, orange, red).
+ */
+
+export const trafficLightZones = {
+  green: [
+    "MOSMAN",
+    "DOUBLE BAY",
+    "VAUCLUSE",
+    "BELLEVUE HILL",
+    "BONDI",
+    "MANLY",
+    "PADDINGTON",
+    "BALMAIN",
+    "NORTH SYDNEY",
+    "RANDWICK",
+    "CHATSWOOD",
+    "CRONULLA"
+  ],
+  orange: [
+    "MARRICKVILLE",
+    "NEWTOWN",
+    "SURRY HILLS",
+    "PARRAMATTA"
+  ],
+  red: [
+    "BLACKTOWN",
+    "MOUNT DRUITT",
+    "LIVERPOOL"
+  ]
+};
+
+export default trafficLightZones;
